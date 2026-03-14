eba190e671a7ba6b7a2cfc70b9ae71a22b39da0f
Manojathirumavalavan <manojathirumavalavan@gmail.com>
2026-03-14 12:21:23 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+input1 = int(input("Enter num1: "))
+input2 = int(input("Enter num2: "))
+input3 = int(input("Enter num3: "))
+digits1 = [int(d) for d in str(input1)]
+digits2 = [int(d) for d in str(input2)]
+digits3 = [int(d) for d in str(input3)]
+max_sum = max(digits1) + max(digits2) + max(digits3)
+min_sum = min(digits1) + min(digits2) + min(digits3)
+key = max_sum * min_sum
+print("Key =", key)
